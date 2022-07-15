88e1298a67c35288dcc9de74a9b9159b8135f91e
Emme <menezes.ecd@gmail.com>
2022-07-15 14:32:39 -0300

    Muda struct de fileData e adiciona equIdentifier

--- include/datatypes.hpp
+++ include/datatypes.hpp
@@ -2,10 +2,22 @@
 #define DATATYPES_H
 
 #include <string>
+#include <vector>
+#include <sstream>
 
 struct fileData {
     std::string name;
-    std::string content;
+    std::stringstream content;
+};
+
+struct tokenMatrix {
+    int lines;
+    std::vector<std::vector<std::string>> matrix;
+};
+
+struct equIdentifier {
+    std::string name;
+    int value;
 };
 
 #endif
