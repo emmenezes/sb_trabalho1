8157d75a2c1748b3acdb4668292636aa76e7b9dd
Emme <menezes.ecd@gmail.com>
2022-07-24 17:44:39 -0300

    Melhora funções do parser de identificação de identificadores sequenciais e quantidade de operandos

--- src/parser.cpp
+++ src/parser.cpp
@@ -13,30 +13,30 @@ void CheckSequentialIdentifiers(tokenMatrix * matrix, std::vector<compilationErr
     bool prev_is_symbol = false;
     compilationError error;
     error.type = "Sintatico";
-    error.message = "Dois ou mais identificadores em sequencia";
 
     for (int i = 0; i < matrix->lines; i++){
         matrix_line = matrix->matrix[i];
         for (int j = 0; j < matrix_line.size(); j++){
             if (matrix_line[j][matrix_line[j].size()-1] == ':'){
                 if (prev_is_symbol){
+                    error.message = "Dois ou mais identificadores em sequencia";
                     error.line = i;
                     error_list->push_back(error);
                 } 
                 prev_is_symbol = true;
-            } else {
-                prev_is_symbol = false;
-            }
-            if ((matrix_line[j] == "SECAO") && (prev_is_symbol)){
                 error.line = i;
+            } else if ((matrix_line[j] == "SECAO") && (prev_is_symbol)){
+                error.message = "Identificador sem atribuição";
                 error_list->push_back(error);
                 prev_is_symbol = false;
-            }
+            } else {
+                prev_is_symbol = false;
+            } 
         }
     }
     if (prev_is_symbol){
         error.message = "Identificador sem atribuição";
-        error.line = matrix->lines - 1;
+        error.line = matrix->lines;
         error_list->push_back(error);
     }
 }
@@ -52,6 +52,8 @@ void CheckOperations(tokenMatrix * matrix, std::vector<compilationError> * error
     for (int i = 0; i < matrix->lines; i++){
         matrix_line = matrix->matrix[i];
         // Checa se é identificador
+        if (matrix_line.size() == 0)
+            continue;
         if (matrix_line[0] == "SECAO")
             continue; 
         if (matrix_line[0][matrix_line[0].size()-1] == ':'){
@@ -64,8 +66,14 @@ void CheckOperations(tokenMatrix * matrix, std::vector<compilationError> * error
             op_quant = matrix_line.size()-1;
         }
         it = op_args_map.find(op);
+        if (it == op_args_map.end()){
+            error.message = "Operacao " + op + " nao identificada, com " + std::to_string(op_quant) + " operando(s)";
+            error.line = i;
+            error_list->push_back(error);
+            continue;
+        }
         if (it->second != op_quant){
-            error.message = "Quantidade de operando incorreta na operacao " + op + ", com " + std::to_string(op_quant) + " operandos";
+            error.message = "Quantidade de operando incorreta na operacao " + op + ", com " + std::to_string(op_quant) + " operando(s)";
             error.line = i;
             error_list->push_back(error);
             continue;
