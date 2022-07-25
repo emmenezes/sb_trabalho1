f3e326255ef177caf966ce21b6856e6565bd1b51
Emme <menezes.ecd@gmail.com>
2022-07-24 22:50:46 -0300

    Pequena correção no mapa de quantidade de argumentos por operação

--- src/parser.cpp
+++ src/parser.cpp
@@ -3,9 +3,9 @@
 std::map<std::string, int> op_args_map = {
     // Operações
     {"ADD", 1}, {"SUB", 1}, {"MUL", 1}, {"DIV", 1}, {"JMP", 1}, {"JMPN", 1}, {"JMPP", 1},{"JMPZ", 1},
-    {"COPY", 2}, {"LOAD", 1}, {"STORE", 1}, {"INPUT", 1}, {"OUTPUT", 1}, {"STORE", 0 }, 
+    {"COPY", 2}, {"LOAD", 1}, {"STORE", 1}, {"INPUT", 1}, {"OUTPUT", 1}, {"STOP", 0 }, 
     // Diretivas
-    {"SPACE", 0}, {"CONST", 1}, {"BEGIN", 0}, {"END", 0}, {"EXTERN", 0}, {"PUBLIC", 0}
+    {"SPACE", 0}, {"CONST", 1}, {"BEGIN", 0}, {"END", 0}, {"EXTERN", 0}, {"PUBLIC", 1}
     };
 
 void CheckSequentialIdentifiers(tokenMatrix * matrix, std::vector<compilationError> * error_list){
